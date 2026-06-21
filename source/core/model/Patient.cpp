0c412d286e7ca4ff4c81dc0d0e5b426ceac9f10a
xcyxiner <593091948@qq.com>
2026-06-21 21:59:30 +0800

    模型赋值

--- source/core/model/Patient.cpp
+++ source/core/model/Patient.cpp
@@ -1,6 +1,29 @@
 
+#include <algorithm>
+#include <iterator>
+#include <memory>
+
 #include "Patient.h"
 
-Patient::Patient()
+Patient::Patient() {}
+
+Study* Patient::addStudy(std::unique_ptr<Study> t_study)
 {
+  auto index = findStudyIndex(t_study.get());
+  if (index == m_studies.size()) {
+    m_studies.emplace_back(std::move(t_study));
+    index = m_studies.size() - 1;
+  }
+  m_studies.at(index)->setIndex(index);
+  return m_studies.at(index).get();
+}
+
+std::size_t Patient::findStudyIndex(Study* t_study)
+{
+  const auto it =
+      std::find_if(m_studies.begin(),
+                   m_studies.end(),
+                   [&t_study](const std::unique_ptr<Study>& study)
+                   { return t_study->getUID() == study->getUID(); });
+  return std::distance(m_studies.begin(), it);
 }
