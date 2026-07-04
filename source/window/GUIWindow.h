8d6a91415a5dd328f07728df43be4358f7ea9725
xcyxiner <593091948@qq.com>
2026-07-04 21:11:15 +0800

    创建ui并编译

--- source/window/GUIWindow.h
+++ source/window/GUIWindow.h
@@ -6,11 +6,17 @@
 #include "qtmetamacros.h"
 #include "qwidget.h"
 
+namespace Ui
+{
+class GUIWindow;
+}
+
 class GUIWindow : public QMainWindow
 {
   Q_OBJECT
 public:
   explicit GUIWindow(QWidget* parent = nullptr);
+  ~GUIWindow();
 
 public:
   void setContent(QWidget* t_widget);
@@ -23,4 +29,6 @@ private:
   QWidget* m_childWidget = {};
 
   FileMenu* m_fileMenu;
+
+  std::unique_ptr<Ui::GUIWindow> ui;
 };
