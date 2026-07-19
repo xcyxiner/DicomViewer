f708d88ab3ac0ebc5302d1644ff6175fbba86571
xcyxiner <593091948@qq.com>
2026-07-19 13:21:00 +0800

    测试vtk

--- 2.sh
+++ 2.sh
@@ -1,2 +1,4 @@
 #~/vcpkg/vcpkg install  --triplet x64-linux --overlay-triplets=./triplets --overlay-ports=./ports-overlay --recurse --no-binarycaching
-~/vcpkg/vcpkg install  --triplet x64-linux --overlay-triplets=./triplets 
+export VCPKG_MAX_CONCURRENCY=6
+#~/vcpkg/vcpkg install  --triplet x64-linux --overlay-triplets=./triplets --overlay-ports=./ports-overlay 
+~/vcpkg/vcpkg install  --triplet x64-linux --overlay-triplets=./triplets
