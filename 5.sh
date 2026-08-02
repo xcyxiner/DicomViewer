638cc10b176af3e019c251c5236172852669b683
xcyxiner <593091948@qq.com>
2026-08-02 18:15:58 +0800

    更新ci打包

---
+++ 5.sh
@@ -0,0 +1,3 @@
+export CMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake
+export VCPKG_ROOT=~/vcpkg
+cmake --preset ci-ubuntu
--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -1,7 +1,21 @@
 install(
     TARGETS DicomViewer_exe
-    RUNTIME COMPONENT DicomViewer_Runtime
+    RUNTIME_DEPENDENCY_SET runtime_deps
+    RUNTIME DESTINATION bin
 )
+
+install(RUNTIME_DEPENDENCY_SET runtime_deps
+    DESTINATION bin
+    COMPONENT DicomViewer_Runtime   # 可选：和 exe 归到同一个 component
+)
+# 在已有的 install() 规则后面加
+if(AUTO_DEPLOY_FONTS AND UNIX)
+    # 安装字体到 prefix/bin/fonts/
+    install(DIRECTORY "${QT_FONT_DEST}"
+        DESTINATION bin/fonts
+        FILES_MATCHING PATTERN "*.ttf"
+    )
+endif()
 
 if(PROJECT_IS_TOP_LEVEL)
   include(CPack)
